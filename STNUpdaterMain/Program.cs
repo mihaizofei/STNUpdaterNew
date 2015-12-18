fd4d3ba7203a37100771e9f8433bd9fd5fda894c
c.m.zofei <mihai@netrom.ro>
2015-12-18 08:47:30 +0200

    Put all products in a list

--- STNUpdaterMain/Program.cs
+++ STNUpdaterMain/Program.cs
@@ -12,6 +12,7 @@ namespace STNUpdater
         static void Main()
         {
             var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "resources", "oferte", "oferta_NOD_2015_12_08.xlsx");
+            var products = new List<Product>();
 
             var connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=\"Excel 12.0;IMEX=1;HDR=NO;TypeGuessRows=0;ImportMixedTypes=Text\""; ;
             using (var conn = new OleDbConnection(connectionString))
@@ -21,19 +22,41 @@ namespace STNUpdater
                 var dt = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
                 List<string> sheetNames = new List<string>();
                 sheetNames.AddRange(dt.Rows.Cast<DataRow>().Select(row => row["TABLE_NAME"].ToString()).Where(tableName => tableName.EndsWith("$") || tableName.EndsWith("$'")));
-                using (var cmd = conn.CreateCommand())
+                foreach (var sheetName in sheetNames)
                 {
-                    cmd.CommandText = "SELECT * FROM [" + sheetNames[0] + "] ";
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT * FROM [" + sheetName + "] ";
 
-                    var adapter = new OleDbDataAdapter(cmd);
-                    var ds = new DataSet();
-                    adapter.Fill(ds);
+                        var adapter = new OleDbDataAdapter(cmd);
+                        var ds = new DataSet();
+                        adapter.Fill(ds);
+                        products.AddRange(MapDataSetToProducts(ds));
+                    }
                 }
             }
 
 
             Console.WriteLine(fileName);
             Console.ReadLine();
+        }
+
+        private static IEnumerable<Product> MapDataSetToProducts(DataSet ds)
+        {
+            var products = ds.Tables[0].AsEnumerable().Select(dataRow => new Product
+                                                                         {
+                                                                             Range = dataRow.Field<string>("F1"),
+                                                                             Category = dataRow.Field<string>("F2"),
+                                                                             Subcategory = dataRow.Field<string>("F3"),
+                                                                             Maker = dataRow.Field<string>("F4"),
+                                                                             Code = dataRow.Field<string>("F5"),
+                                                                             Description = dataRow.Field<string>("F6"),
+                                                                             Price = dataRow.Field<string>("F7"),
+                                                                             Currency = dataRow.Field<string>("F8"),
+                                                                             Warranty = dataRow.Field<string>("F10")
+                                                                         }).ToList();
+
+            return products;
         }
     }
 }
